c478385949dbaf1b48516b0cb4b87fb3b5a16347
Robert (Jamie) Munro <rjmunro@arjam.net>
2012-02-11 15:34:43 +0000

    Began KlassDetailView

--- inspector/cbv/views.py
+++ inspector/cbv/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+from django.views.generic import DetailView
+from cbv.models import Klass
+from django.http import HttpResponse
+from pprint import pformat,pprint
+
+class KlassDetailView (DetailView):
+	model = Klass
+	def get_object(self): #, request, klass, module, package, version):
+		pprint(self.kwargs)
+		return self.model.objects.get(
+				name__iexact=self.kwargs['klass'],
+				module__name__iexact=self.kwargs['module'],
+				module__project_version__version_number__iexact=self.kwargs['version'],
+				module__project_version__project__name__iexact=self.kwargs['package'],
+				)
+
--- inspector/inspector/urls.py
+++ inspector/inspector/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 
+from cbv.views import KlassDetailView
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -11,6 +13,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
+	url(r'class/(?P<package>[a-zA-Z_-]+)/(?P<version>[^/]+)/(?P<module>[A-Za-z_-]+)/(?P<klass>[A-Za-z_-]*)', KlassDetailView.as_view()),
 )
 
 urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
