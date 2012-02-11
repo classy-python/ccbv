d86f475ce9b4e813fcd3f616c0a02632faaa5c53
Robert (Jamie) Munro <rjmunro@arjam.net>
2012-02-11 19:25:54 +0000

    Split the cbv URLs into their own file.

---
+++ inspector/cbv/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+
+from cbv.views import KlassDetailView, KlassListView
+
+urlpatterns = patterns('',
+	url(r'^(?P<package>[a-zA-Z_-]+)/(?P<version>[^/]+)/(?P<module>[\.A-Za-z_-]+)/$', KlassListView.as_view(), name='module-detail'),
+	url(r'^(?P<package>[a-zA-Z_-]+)/(?P<version>[^/]+)/(?P<module>[\.A-Za-z_-]+)/(?P<klass>[A-Za-z_-]*)/$', KlassDetailView.as_view(), name='klass-detail'),
+)
