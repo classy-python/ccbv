463aeabbcbcb41798e2aae8373e9c0affbb08ca9
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-02-11 22:07:12 +0000

    Separate the fuzzy matching into a mixin.

--- inspector/cbv/views.py
+++ inspector/cbv/views.py
@@ -4,6 +4,23 @@ from cbv.models import Klass, Module, ProjectVersion
 from django.http import HttpResponse,HttpResponseRedirect
 from pprint import pformat,pprint
 
+
+class FuzzySingleObjectMixin(SingleObjectMixin):
+
+    def get_object(self, queryset=None):
+        try:
+            return self.get_precise_object(queryset)
+        except self.model.DoesNotExist:
+            obj = self.get_fuzzy_object(queryset)
+            self.push_state_url = obj.get_absolute_url()
+            return obj
+
+    def get_context_data(self, **kwargs):
+        context = super(ModuleDetailView, self).get_context_data(**kwargs)
+        context['push_state_url'] = self.push_state_url
+        return context
+
+
 class KlassDetailView(DetailView):
     model = Klass
 
@@ -19,30 +36,23 @@ class KlassDetailView(DetailView):
         )
 
 
-class ModuleDetailView(DetailView):
+class ModuleDetailView(FuzzySingleObjectMixin, DetailView):
     model = Module
     push_state_url = None
 
-    def get_object(self):
-        try: 
-            return self.model.objects.get(
-                name=self.kwargs['module'],
-                project_version__version_number=self.kwargs['version'],
-                project_version__project__name=self.kwargs['package'],
-            )
-        except self.model.DoesNotExist:
-            obj = self.model.objects.get(
-                name__iexact=self.kwargs['module'],
-                project_version__version_number__iexact=self.kwargs['version'],
-                project_version__project__name__iexact=self.kwargs['package'],
-            )
-            self.push_state_url = obj.get_absolute_url()
-            return obj
+    def get_precise_object(self, queryset=None):
+        return self.model.objects.get(
+            name=self.kwargs['module'],
+            project_version__version_number=self.kwargs['version'],
+            project_version__project__name=self.kwargs['package'],
+        )
 
-    def get_context_data(self, **kwargs):
-        context = super(ModuleDetailView, self).get_context_data(**kwargs)
-        context['push_state_url'] = self.push_state_url
-        return context
+    def get_fuzzy_object(self, queryset=None):
+        return self.model.objects.get(
+            name__iexact=self.kwargs['module'],
+            project_version__version_number__iexact=self.kwargs['version'],
+            project_version__project__name__iexact=self.kwargs['package'],
+        )
 
 
 class VersionDetailView(DetailView):
