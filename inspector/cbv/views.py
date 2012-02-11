989287bf23aed65264c0f8398c8dff8001f134d5
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-02-11 16:48:03 +0000

    Make the class list a module detail view.

--- inspector/cbv/views.py
+++ inspector/cbv/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
-from django.views.generic import DetailView, ListView
-from cbv.models import Klass
+from django.views.generic import DetailView
+from cbv.models import Klass, Module
 from django.http import HttpResponse
 from pprint import pformat,pprint
 
@@ -15,11 +15,11 @@ class KlassDetailView(DetailView):
 				module__project_version__project__name__iexact=self.kwargs['package'],
 				)
 
-class KlassListView(ListView):
-    model = Klass
-    def get_queryset(self):
-        return self.model.objects.filter(
-            module__name__iexact=self.kwargs['module'],
-            module__project_version__version_number__iexact=self.kwargs['version'],
-            module__project_version__project__name__iexact=self.kwargs['package'],
+class KlassListView(DetailView):
+    model = Module
+    def get_object(self):
+        return self.model.objects.get(
+            name__iexact=self.kwargs['module'],
+            project_version__version_number__iexact=self.kwargs['version'],
+            project_version__project__name__iexact=self.kwargs['package'],
         )
