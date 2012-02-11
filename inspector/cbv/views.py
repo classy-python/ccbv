6f0518390a01a9d1074905e6d58848049439559e
Robert (Jamie) Munro <rjmunro@arjam.net>
2012-02-11 19:40:05 +0000

    Added VersionDetailView to show all versions of a project that have been indexed.

--- inspector/cbv/views.py
+++ inspector/cbv/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.views.generic import DetailView
-from cbv.models import Klass, Module
+from cbv.models import Klass, Module, ProjectVersion
 from django.http import HttpResponse
 from pprint import pformat,pprint
 
@@ -27,4 +27,14 @@ class ModuleDetailView(DetailView):
             name__iexact=self.kwargs['module'],
             project_version__version_number__iexact=self.kwargs['version'],
             project_version__project__name__iexact=self.kwargs['package'],
+        )
+
+
+class VersionDetailView(DetailView):
+    model = ProjectVersion
+
+    def get_object(self):
+        return self.model.objects.get(
+            version_number__iexact=self.kwargs['version'],
+            project__name__iexact=self.kwargs['package'],
         )
