e4969567a3abe62ef9da959dc02b83a492a22e33
Charlie Denton <charlie@meshy.co.uk>
2012-03-12 14:16:41 +0000

    Make methods appear in MRO on KlassDetailView.

--- inspector/cbv/templatetags/cbv_tags.py
+++ inspector/cbv/templatetags/cbv_tags.py
@@ -5,5 +5,22 @@ register = template.Library()
 
 
 @register.filter
-def called(qs, name):
-    return [item for item in qs if item.name==name]
+def namesake_methods(parent_klass, name):
+    namesakes = [m for m in parent_klass.get_methods() if m.name==name]
+    assert(namesakes)
+    # Get the methods in order of the klasses
+    try:
+        result = [next((m for m in namesakes if m.klass==parent_klass))]
+        namesakes.pop(namesakes.index(result[0]))
+    except StopIteration:
+        result = []
+    for klass in parent_klass.get_all_ancestors():
+        # Move the namesakes from the methods to the results
+        try:
+            method = next((m for m in namesakes if m.klass==klass))
+            namesakes.pop(namesakes.index(method))
+            result.append(method)
+        except StopIteration:
+            pass
+    assert(not namesakes)
+    return result
