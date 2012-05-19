b403a4364e7e1d57858ad2731ae886da6edb8cf2
Charlie Denton <charlie@meshy.co.uk>
2012-05-19 13:39:10 +0100

    PEP8.

--- inspector/cbv/templatetags/cbv_tags.py
+++ inspector/cbv/templatetags/cbv_tags.py
@@ -1,23 +1,22 @@
 from django import template
-from django.conf import settings
 
 register = template.Library()
 
 
 @register.filter
 def namesake_methods(parent_klass, name):
-    namesakes = [m for m in parent_klass.get_methods() if m.name==name]
+    namesakes = [m for m in parent_klass.get_methods() if m.name == name]
     assert(namesakes)
     # Get the methods in order of the klasses
     try:
-        result = [next((m for m in namesakes if m.klass==parent_klass))]
+        result = [next((m for m in namesakes if m.klass == parent_klass))]
         namesakes.pop(namesakes.index(result[0]))
     except StopIteration:
         result = []
     for klass in parent_klass.get_all_ancestors():
         # Move the namesakes from the methods to the results
         try:
-            method = next((m for m in namesakes if m.klass==klass))
+            method = next((m for m in namesakes if m.klass == klass))
             namesakes.pop(namesakes.index(method))
             result.append(method)
         except StopIteration:
