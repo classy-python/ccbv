5b70407a09bc67386b70516dd0f448564a9f2d79
Michael Blatherwick <michael@wildknowledge.co.uk>
2012-08-20 00:17:49 +0100

    Define natural key methods
    For all relevent models, define natural_key() on the model and
    get_by_natural_key() on the manager.
    
    Had to drop Module.parent self-referencing field as these don't
    play nicely with natural keys; don't think it was being used in
    any views (yet), but could be an issue.

---
+++ cbv/migrations/0011_auto__del_field_module_parent.py
@@ -0,0 +1,95 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Deleting field 'Module.parent'
+        db.delete_column('cbv_module', 'parent_id')
+
+
+    def backwards(self, orm):
+        
+        # Adding field 'Module.parent'
+        db.add_column('cbv_module', 'parent', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cbv.Module'], null=True, blank=True), keep_default=False)
+
+
+    models = {
+        'cbv.function': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Function'},
+            'code': ('django.db.models.fields.TextField', [], {}),
+            'docstring': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'kwargs': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'line_number': ('django.db.models.fields.IntegerField', [], {}),
+            'module': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cbv.Module']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'cbv.inheritance': {
+            'Meta': {'ordering': "('order',)", 'unique_together': "(('child', 'order'),)", 'object_name': 'Inheritance'},
+            'child': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'ancestor_relationships'", 'to': "orm['cbv.Klass']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cbv.Klass']"})
+        },
+        'cbv.klass': {
+            'Meta': {'unique_together': "(('module', 'name'),)", 'object_name': 'Klass'},
+            'docstring': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'import_path': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'line_number': ('django.db.models.fields.IntegerField', [], {}),
+            'module': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cbv.Module']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'cbv.klassattribute': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('klass', 'name'),)", 'object_name': 'KlassAttribute'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'klass': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'attribute_set'", 'to': "orm['cbv.Klass']"}),
+            'line_number': ('django.db.models.fields.IntegerField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'cbv.method': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Method'},
+            'code': ('django.db.models.fields.TextField', [], {}),
+            'docstring': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'klass': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cbv.Klass']"}),
+            'kwargs': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'line_number': ('django.db.models.fields.IntegerField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'cbv.module': {
+            'Meta': {'unique_together': "(('project_version', 'name'),)", 'object_name': 'Module'},
+            'docstring': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'filename': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '511'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'project_version': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cbv.ProjectVersion']"})
+        },
+        'cbv.moduleattribute': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('module', 'name'),)", 'object_name': 'ModuleAttribute'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'line_number': ('django.db.models.fields.IntegerField', [], {}),
+            'module': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'attribute_set'", 'to': "orm['cbv.Module']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'cbv.project': {
+            'Meta': {'object_name': 'Project'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'})
+        },
+        'cbv.projectversion': {
+            'Meta': {'ordering': "('-version_number',)", 'unique_together': "(('project', 'version_number'),)", 'object_name': 'ProjectVersion'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cbv.Project']"}),
+            'version_number': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['cbv']
