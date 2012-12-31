e43376f4d71da7a015fd588fdb03f1695994f3d2
Charlie Denton <charlie@meshy.co.uk>
2012-12-31 10:33:02 +0000

    Dump version data in a specific order.

--- cbv/management/commands/cbv_dumpversion.py
+++ cbv/management/commands/cbv_dumpversion.py
@@ -10,17 +10,18 @@ from cbv import models
 class Command(LabelCommand):
     """Dump the django cbv app data for a specific version."""
     def handle_label(self, label, **options):
-        filtered_models = {
-            models.ProjectVersion: 'version_number',
-            models.Module: 'project_version__version_number',
-            models.ModuleAttribute: 'module__project_version__version_number',
-            models.Function: 'module__project_version__version_number',
-            models.Klass: 'module__project_version__version_number',
-            models.KlassAttribute: 'klass__module__project_version__version_number',
-            models.Method: 'klass__module__project_version__version_number',
-        }
+        filtered_models = (
+            (models.ProjectVersion, 'version_number'),
+            (models.Module, 'project_version__version_number'),
+            (models.ModuleAttribute, 'module__project_version__version_number'),
+            (models.Function, 'module__project_version__version_number'),
+            (models.Klass, 'module__project_version__version_number'),
+            (models.KlassAttribute, 'klass__module__project_version__version_number'),
+            (models.Method, 'klass__module__project_version__version_number'),
+            (models.Inheritance, 'parent__module__project_version__version_number'),
+        )
         objects = []
-        for model, version_arg in filtered_models.items():
+        for model, version_arg in filtered_models:
             filter_kwargs = {version_arg: label}
             result = model.objects.filter(**filter_kwargs)
             objects = objects + list(result)
