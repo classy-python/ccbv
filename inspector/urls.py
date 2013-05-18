1380dd690836bab72b200c976490b223afe6029b
Kevin Van Wilder <kevin@inuits.eu>
2013-05-18 12:34:19 +0200

    Implemented #22: Make a proper 404 page.

--- inspector/urls.py
+++ inspector/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 from cbv.views import HomeView
 
@@ -17,3 +18,9 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^404/', TemplateView.as_view(template_name='404.html')),
+        url(r'^500/', TemplateView.as_view(template_name='500.html')),
+    )
