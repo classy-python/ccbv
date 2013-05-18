d2f2ff644e1a6af43a105125c83d9cc57c87616c
Charlie Denton <charlie@meshy.co.uk>
2013-05-18 11:31:25 +0100

    Import order and whitespace in urls file.

--- inspector/urls.py
+++ inspector/urls.py
@@ -1,19 +1,18 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from cbv.views import HomeView
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
+
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^projects/', include('cbv.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cbv.shortcut_urls'), {'package': 'Django'}),
-)
-
-urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
