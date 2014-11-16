0dd81c6448f344816103ea15bd58f3f04d331fd2
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-11-16 14:07:15 -0200

    configurations for production, adding django-heroku-memcacheify to get the memcacheify as an env var from heroku

--- inspector/settings/production.py
+++ inspector/settings/production.py
@@ -1,28 +1,34 @@
 from __future__ import absolute_import
 
-from .base import *
+import os
 
-os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS']
-os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
-os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
+from configurations import values
+# See: https://github.com/rdegges/django-heroku-memcacheify
+from memcacheify import memcacheify
 
-CACHES = {
-            'default': {
-                'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-                'LOCATION': os.environ['MEMCACHIER_SERVERS'],
-                'TIMEOUT': 500,
-                'BINARY': True,
-            }
-        }
+from .base import Common
 
 
-INSTALLED_APPS = INSTALLED_APPS + ('gunicorn',)
+class Production(Common):
 
-ALLOWED_HOSTS = ('*',)
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS']
+    os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
+    os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-S3_URL = 'https://{0}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
+    CACHES = memcacheify(timeout=500)
+
+    # SECRET KEY
+    SECRET_KEY = values.SecretValue()
+    # END SECRET KEY
+
+    INSTALLED_APPS = Common.INSTALLED_APPS
+    INSTALLED_APPS += ('gunicorn',)
+
+    ALLOWED_HOSTS = ('*',)
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    S3_URL = 'https://{0}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
