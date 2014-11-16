4ee8733b694b0ce6fac9b9188db3f81e80884c6f
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-11-16 13:23:23 -0200

    adding django-configuration to requirements and changed manage.py to use it

--- manage.py
+++ manage.py
@@ -2,8 +2,9 @@
 import os, sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inspector.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 
-    from django.core.management import execute_from_command_line
+    from configurations.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
