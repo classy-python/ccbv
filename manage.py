85d1f80e84b17665947423e8837188551ed7b8db
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-11-16 14:05:12 -0200

    adding django-configuration to manage.py and wsgi, to load info from the envs

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os, sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inspector.settings")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 
     from configurations.management import execute_from_command_line
