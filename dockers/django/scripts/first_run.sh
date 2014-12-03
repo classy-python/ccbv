f6974a63fed9b580a3bc68812f1d8e140cdc7107
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-12-03 04:00:24 -0200

    making the django docker start and run. Just missing some other (env)config vars like the static assets

--- dockers/django/scripts/first_run.sh
+++ dockers/django/scripts/first_run.sh
@@ -1,22 +1,7 @@
 
 pre_start_action() {
-    echo "installing requirements.txt"
-    pip install -r $DATA_DIR/requirements.txt
-
-
-    if [ -z $DB_PORT ];then
-        echo "DB_PORT env var not configured, link a Postgresql DB container with alias: db on port 5432"
-        exit 1
-    fi
-
-    echo "Waiting for DB to be ready"
-    while ! exec 6<>/dev/tcp/${DB_PORT_5432_TCP_ADDR}/${DB_PORT_5432_TCP_PORT}; do
-        echo "$(date) - still trying to connect to DB at ${DB_PORT}"
-        sleep 1
-    done
-
-    echo "settings DATABASE_URL env var"
-    export DATABASE_URL=$DB_PORT
+    echo "Ensure DB is clear"
+    python $DATA_DIR/manage.py reset_db --router=default --noinput
 
     echo "Syncing DB"
     python $DATA_DIR/manage.py syncdb --noinput
@@ -36,8 +21,8 @@ pre_start_action() {
 
     echo "Creating Superuser. User: $USER / Email: $USER_EMAIL / Pass: $USER_PASS"
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('$USER', '$USER_EMAIL', '$USER_PASS')" | \
-    python manage.py shell
+    python $DATA_DIR/manage.py shell
 
-    touch $DATA_DIR/firstrun
+    touch /steps/firstrun
 }
 
--- dockers/django/scripts/start.sh
+++ dockers/django/scripts/start.sh
@@ -6,7 +6,24 @@ set -e
 
 DATA_DIR=/data
 
-if [[ -e $DATA_DIR/firstrun ]]; then
+# SET DATABASE_URL env var
+export DATABASE_URL="postgres://$DB_ENV_USER:$DB_ENV_PASS@db:$DB_PORT_5432_TCP_PORT/$DB_ENV_DB"
+
+echo "trying to update if needed requirements.txt"
+pip install -r $DATA_DIR/requirements.txt
+
+if [ -z $DB_PORT ];then
+    echo "DB env var not configured, link a Postgresql DB container with alias: db on port 5432"
+    exit 1
+fi
+
+echo "Waiting for DB to be ready"
+while ! exec 6<>/dev/tcp/${DB_PORT_5432_TCP_ADDR}/${DB_PORT_5432_TCP_PORT}; do
+    echo "$(date) - still trying to connect to DB at ${DB_PORT}"
+    sleep 1
+done
+
+if [[ -e /steps/firstrun ]]; then
   source /scripts/normal_run.sh
 else
   source /scripts/first_run.sh
@@ -17,4 +34,4 @@ pre_start_action
 
 # Django
 echo "Starting Django..."
-python $DATA_DIR/manage.py runserver
+python $DATA_DIR/manage.py runserver 0.0.0.0:8000
