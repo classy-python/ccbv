d92c56f6826b4bbb5d47184f98ee3d8a39b8ec7e
George Hickman <george@ghickman.co.uk>
2016-05-10 16:46:11 +0100

    Modernise urls.py modules

--- inspector/urls.py
+++ inspector/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -11,17 +11,17 @@ from cbv.views import HomeView, Sitemap
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^projects/', include('cbv.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^sitemap\.xml$', Sitemap.as_view(), name='sitemap'),
     url(r'^', include('cbv.shortcut_urls'), {'package': 'Django'}),
-) + staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^404/$', TemplateView.as_view(template_name='404.html')),
         url(r'^500/$', TemplateView.as_view(template_name='500.html')),
-    )
+    ]
