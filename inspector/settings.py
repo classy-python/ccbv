a9a8e05860990843be574d83018f42de29295788
George Hickman <george@ghickman.co.uk>
2016-05-10 21:43:08 +0100

    Switch to django cache url

--- inspector/settings.py
+++ inspector/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 import dj_database_url
-from memcacheify import memcacheify
+import django_cache_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -90,7 +90,7 @@ DATABASES = {'default': dj_database_url.config(default='postgres://localhost/ccb
 
 # Caching
 # https://docs.djangoproject.com/en/1.9/ref/settings/#caches
-CACHES = memcacheify()
+CACHES = {'default': django_cache_url.config()}
 
 
 # Password validation
