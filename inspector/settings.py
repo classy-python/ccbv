ba20122369da56d7280d860b5095977c17ce5d75
George Hickman <george@ghickman.co.uk>
2016-05-11 08:49:09 +0100

    Turn off all timezone and localisation since it's unused

--- inspector/settings.py
+++ inspector/settings.py
@@ -113,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 LANGUAGE_CODE = 'en-GB'
 TIME_ZONE = 'Europe/London'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
+USE_I18N = False
+USE_L10N = False
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
