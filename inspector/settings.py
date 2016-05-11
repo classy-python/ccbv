53570f2fb141e0b25ce80946f3bbb7ac4a460e86
George Hickman <george@ghickman.co.uk>
2016-05-11 08:48:29 +0100

    Remove unnecessary setting

--- inspector/settings.py
+++ inspector/settings.py
@@ -122,9 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    'static',
-]
 
 
 # PROJECT SETTINGS
