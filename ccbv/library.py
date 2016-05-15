0ea411561d52b570e8e61acf53f59bc258c13a64
George Hickman <george@ghickman.co.uk>
2016-05-15 17:20:16 +0100

    Sort attrs&methods by name
    Now that we sort attributes and methods once they've all been generated
    we can remove DefaultOrderedDict.

--- ccbv/library.py
+++ ccbv/library.py
@@ -4,51 +4,6 @@ import inspect
 import pydoc
 
 
-class DefaultOrderedDict(collections.OrderedDict):
-    def __init__(self, default_factory=None, *a, **kw):
-        if (default_factory is not None and
-                not isinstance(default_factory, collections.Callable)):
-            raise TypeError('first argument must be callable')
-        collections.OrderedDict.__init__(self, *a, **kw)
-        self.default_factory = default_factory
-
-    def __getitem__(self, key):
-        try:
-            return collections.OrderedDict.__getitem__(self, key)
-        except KeyError:
-            return self.__missing__(key)
-
-    def __missing__(self, key):
-        if self.default_factory is None:
-            raise KeyError(key)
-        self[key] = value = self.default_factory()
-        return value
-
-    def __reduce__(self):
-        if self.default_factory is None:
-            args = tuple()
-        else:
-            args = self.default_factory,
-        return type(self), args, None, None, self.items()
-
-    def copy(self):
-        return self.__copy__()
-
-    def __copy__(self):
-        return type(self)(self.default_factory, self)
-
-    def __deepcopy__(self, memo):
-        import copy
-        return type(self)(self.default_factory,
-                          copy.deepcopy(self.items()))
-
-    def __repr__(self):
-        return 'OrderedDefaultDict({0}, {1})'.format(
-            self.default_factory,
-            collections.OrderedDict.__repr__(self),
-        )
-
-
 def classify(klass, obj, name=None, mod=None, *ignored):
     if not inspect.isclass(obj):
         raise Exception
@@ -100,14 +55,18 @@ def classify(klass, obj, name=None, mod=None, *ignored):
             }
             method.append(data)
 
+    # sort attributes & methods
+    klass['attributes'] = collections.OrderedDict(sorted(klass['attributes'].items()))
+    klass['methods'] = collections.OrderedDict(sorted(klass['methods'].items(), key=lambda m: m[0].strip('__')))
+
     return klass
 
 
 def build(thing):
     """Build a dictionary mapping of a class."""
     klass = {
-        'attributes': DefaultOrderedDict(list),
-        'methods': DefaultOrderedDict(list),
+        'attributes': collections.defaultdict(list),
+        'methods': collections.defaultdict(list),
         'properties': [],
         'ancestors': [],
         'parents': [],
