d0e6353434fe7d47462c43a17a48723835e5a5ae
Martin Koistinen <mkoistinen@gmail.com>
2017-05-04 17:09:16 -0400

    Work around for lack of signal for collapsing of all methods

--- cbv/static/permalinks.js
+++ cbv/static/permalinks.js
@@ -2,18 +2,48 @@
 (function(doc){
     "use strict";
 
+    // On DOM Load...
     $(function(){
-        var $section, hash = window.location.hash;
+        var hash = window.location.hash;
+        var headerHeight = $('.navbar').height();
+        var methods = $('.accordion-group .accordion-heading');
+        var methodCount = methods.length;
+
+        // Sets-up an event handler that will expand and scroll to the desired
+        // (hash) method.
         if(hash){
-            $section = $(hash);
-            if($section){
-                $(doc).one('hidden.bs.collapse', hash, function(){
-                    $section.parent().find('h3').click();
-                });
-                $('html, body').animate({
-                        scrollTop: $(hash).offset().top
-                }, 500);
-            }
+            // We need to know that all the panes have (at least initially)
+            // collapsed as a result of loading the page. Unfortunately, we
+            // only get events for each collapsed item, so we count...
+            $(doc).on('hidden.bs.collapse', function(evt) {
+                var methodTop, $hdr = $(hash).parent('.accordion-group');
+                methodCount--;
+                if(methodCount === 0){
+                    // OK, they've /all/ collapsed, now we expand the one we're
+                    // interested in the scroll to it.
+
+                    // First, remove this very event handler
+                    $(doc).off('hidden.bs.collapse');
+                    // Now wait just a beat more to allow the last collapse
+                    // animation to complete...
+                    setTimeout(function(){
+                        // Open the desired method
+                        $hdr.find('h3').click();
+                        // Take into account the fixed header and the margin
+                        methodTop = $hdr.offset().top - headerHeight - 8;
+                        // Scroll it into view
+                        $('html, body').animate({scrollTop: methodTop}, 250);
+                    }, 250);
+                }
+            });
         }
+
+        // Delegated event handler to prevent the collapse/expand function of
+        // a method's header when clicking the Pilcrow (permalink)
+        $('.accordion-group').on('click', '.permalink', function(evt){
+            evt.preventDefault();
+            evt.stopImmediatePropagation();
+            window.location = $(this).attr('href');
+        });
     })
 })(document);
