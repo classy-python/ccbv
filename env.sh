2138cad256549ce7ae71db11a5c7df6494851472
George Hickman <george@ghickman.co.uk>
2018-08-23 12:58:44 +0100

    Remove Django version of the inspector

--- env.sh
+++
@@ -1,6 +0,0 @@
-# gets directory path, required to set up sqlite db file
-DIR_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-
-export DEBUG='True'
-export DATABASE_URL='sqlite:///'$DIR_PATH'/db.sqlite'
-export STATIC_URL='/static_media/'
