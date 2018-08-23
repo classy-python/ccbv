e1d632a8e48aa0dca5a21de1aa1b34fc4e66502e
George Hickman <george@ghickman.co.uk>
2018-08-23 12:58:44 +0100

    Remove Django version of the inspector

--- cbv/shortcut_urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from cbv import views
-
-urlpatterns = [
-    url(r'(?P<klass>[a-zA-Z_-]+)/$', views.LatestKlassDetailView.as_view(), name='klass-detail-shortcut'),
-]
