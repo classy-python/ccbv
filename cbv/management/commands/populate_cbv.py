ab10e58fd91a35ea9d05d876983ad2826d358019
Charlie Denton <charlie@meshy.co.uk>
2021-12-02 19:18:50 +0000

    Move function calls into subfunctions

--- cbv/management/commands/populate_cbv.py
+++ cbv/management/commands/populate_cbv.py
@@ -177,7 +177,14 @@ class CBVImporter:
                 docstring=get_docstring(module),
                 filename=filename,
             )
-            return this_node
+            yield this_node
+            if this_node:
+                # Go through members
+                yield from self._process_submembers(
+                    root_module_name=root_module_name,
+                    parent=module,
+                    parent_node=this_node,
+                )
 
         def handle_class_on_module(member, member_name, parent, parent_node):
             if inspect.getsourcefile(member) != inspect.getsourcefile(parent):
@@ -198,7 +205,14 @@ class CBVImporter:
                 import_path=import_path,
             )
             self.klasses[member] = this_node
-            return this_node
+            yield this_node
+            if this_node:
+                # Go through members
+                yield from self._process_submembers(
+                    root_module_name=root_module_name,
+                    parent=member,
+                    parent_node=this_node,
+                )
 
         def handle_function_or_method(member, member_name, parent, parent_node):
             # Decoration
@@ -246,27 +260,12 @@ class CBVImporter:
 
         # MODULE
         if inspect.ismodule(member):
-            this_node = handle_module(member, root_module_name)
-            yield this_node
-            if this_node:
-                # Go through members
-                yield from self._process_submembers(
-                    root_module_name=root_module_name,
-                    parent=member,
-                    parent_node=this_node,
-                )
+            yield from handle_module(member, root_module_name)
+            return
 
         # CLASS
         elif inspect.isclass(member) and inspect.ismodule(parent):
-            this_node = handle_class_on_module(member, member_name, parent, parent_node)
-            yield this_node
-            if this_node:
-                # Go through members
-                yield from self._process_submembers(
-                    root_module_name=root_module_name,
-                    parent=member,
-                    parent_node=this_node,
-                )
+            yield from handle_class_on_module(member, member_name, parent, parent_node)
 
         # METHOD
         elif inspect.ismethod(member) or inspect.isfunction(member):
