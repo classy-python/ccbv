d06f7207dbae36a3e23f17ab64e91f9b8ca33ac8
Charlie Denton <charlie@meshy.co.uk>
2021-12-18 23:07:12 +0000

    Hint at AAA with test spacing

--- cbv/tests/test_models.py
+++ cbv/tests/test_models.py
@@ -23,6 +23,7 @@ class KlassAncestorMROTest(TestCase):
         c = InheritanceFactory.create(parent=b, child__name="c").child
 
         mro = c.get_all_ancestors()
+
         self.assertSequenceEqual(mro, [b, a])
 
     def test_diamond(self):
@@ -41,10 +42,10 @@ class KlassAncestorMROTest(TestCase):
         b_child_of_a = InheritanceFactory.create(child__name="b", parent__name="a")
         a = b_child_of_a.parent
         b = b_child_of_a.child
-
         c = InheritanceFactory.create(parent=a, child__name="c").child
         d = InheritanceFactory.create(parent=b, child__name="d").child
         InheritanceFactory.create(parent=c, child=d, order=2)
 
         mro = d.get_all_ancestors()
+
         self.assertSequenceEqual(mro, [b, c, a])
--- cbv/tests/test_views.py
+++ cbv/tests/test_views.py
@@ -10,12 +10,16 @@ class SitemapTest(TestCase):
 
     def test_200(self):
         ProjectVersionFactory.create()
+
         response = self.client.get(self.url)
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response["Content-Type"], "application/xml")
 
     def test_queryset(self):
         KlassFactory.create()
         with self.assertNumQueries(2):  # Get ProjectVersion, get Klasses.
+
             url_list = Sitemap().get_queryset()
+
         self.assertEqual(len(url_list), 2)  # 2 because 1 Klass + homepage.
