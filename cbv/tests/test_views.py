9865ea78827382555c91203e7513fa23638a4150
Charlie Denton <charlie@meshy.co.uk>
2021-12-19 00:12:59 +0000

    Separate Django project creation in test

--- cbv/tests/test_views.py
+++ cbv/tests/test_views.py
@@ -6,7 +6,7 @@ from django.test.utils import CaptureQueriesContext
 from django.urls import reverse_lazy
 
 from ..views import Sitemap
-from .factories import KlassFactory, ProjectVersionFactory
+from .factories import KlassFactory, ProjectFactory, ProjectVersionFactory
 
 
 @pytest.mark.django_db
@@ -38,11 +38,12 @@ class TestSitemap:
         assert response.content.decode() == Path(filename).read_text()
 
     def test_populated_content(self, client: Client) -> None:
+        project = ProjectFactory(name="Django")
         KlassFactory.create(
             name="Klass",
             module__name="module.name",
             module__project_version__version_number="42.0",
-            module__project_version__project__name="Django",
+            module__project_version__project=project,
         )
 
         response = client.get(self.url)
