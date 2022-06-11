a44a2f6629a01aac7d1d32068869fbdbc879a87d
Charlie Denton <charlie@meshy.co.uk>
2022-06-11 15:00:06 +0100

    Move variable into only function that uses it

--- cbv/views.py
+++ cbv/views.py
@@ -188,14 +188,11 @@ class VersionDetailView(TemplateView):
 class HomeView(TemplateView):
     template_name = "home.html"
 
-    def get(self, request, *args, **kwargs):
-        self.project_version = self.get_project_version(**kwargs)
-        return super().get(request, *args, **kwargs)
-
     def get_project_version(self, **kwargs):
         return ProjectVersion.objects.get_latest("Django")
 
     def get_context_data(self, **kwargs):
+        self.project_version = self.get_project_version(**kwargs)
         return {
             "object_list": Klass.objects.filter(
                 module__project_version=self.project_version
