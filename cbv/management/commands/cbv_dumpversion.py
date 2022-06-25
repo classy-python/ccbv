90e57c20e517c10d19394f8a0860b755b794291e
Charlie Denton <charlie@meshy.co.uk>
2022-06-25 23:57:04 +0100

    Remove roundabout way of constructing querysets

--- cbv/management/commands/cbv_dumpversion.py
+++ cbv/management/commands/cbv_dumpversion.py
@@ -8,19 +8,23 @@ class Command(LabelCommand):
     """Dump the django cbv app data for a specific version."""
 
     def handle_label(self, label, **options):
-        filtered_models = (
-            (models.ProjectVersion, "version_number"),
-            (models.Module, "project_version__version_number"),
-            (models.Klass, "module__project_version__version_number"),
-            (models.KlassAttribute, "klass__module__project_version__version_number"),
-            (models.Method, "klass__module__project_version__version_number"),
-            (models.Inheritance, "parent__module__project_version__version_number"),
+        querysets = (
+            models.ProjectVersion.objects.filter(version_number=label),
+            models.Module.objects.filter(project_version__version_number=label),
+            models.Klass.objects.filter(module__project_version__version_number=label),
+            models.KlassAttribute.objects.filter(
+                klass__module__project_version__version_number=label
+            ),
+            models.Method.objects.filter(
+                klass__module__project_version__version_number=label
+            ),
+            models.Inheritance.objects.filter(
+                parent__module__project_version__version_number=label
+            ),
         )
         objects = []
-        for model, version_arg in filtered_models:
-            filter_kwargs = {version_arg: label}
-            result = model.objects.filter(**filter_kwargs)
-            objects = objects + list(result)
+        for queryset in querysets:
+            objects = objects + list(queryset)
         for obj in objects:
             obj.pk = None
         dump = serializers.serialize(
