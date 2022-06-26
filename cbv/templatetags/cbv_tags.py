7f1054eda9ee9373f95eda76fe7e84d5f32975c2
Charlie Denton <charlie@meshy.co.uk>
2022-06-26 01:25:25 +0100

    Fetch version name before sending to template

--- cbv/templatetags/cbv_tags.py
+++ cbv/templatetags/cbv_tags.py
@@ -70,6 +70,7 @@ class ModuleData:
 @attrs.frozen
 class NavData:
     projectversion: ProjectVersion
+    version_name: str
     version_number: str
     other_versions: list[OtherVersion]
     modules: list[ModuleData]
@@ -111,6 +112,7 @@ def nav(projectversion, module=None, klass=None):
 
     nav_data = NavData(
         projectversion=projectversion,
+        version_name=str(projectversion),
         version_number=projectversion.version_number,
         other_versions=version_switcher,
         modules=modules,
