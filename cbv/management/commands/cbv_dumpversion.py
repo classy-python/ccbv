51206383b6039659e44af95e27ff8e884c4de23a
Charlie Denton <charlie@meshy.co.uk>
2022-08-14 12:00:19 +0100

    Introduce predictable ordering cbv_dumpversion

--- cbv/management/commands/cbv_dumpversion.py
+++ cbv/management/commands/cbv_dumpversion.py
@@ -11,18 +11,23 @@ class Command(LabelCommand):
 
     def handle_label(self, label, **options):
         querysets = (
+            # There will be only one ProjectVersion, so no need for ordering.
             models.ProjectVersion.objects.filter(version_number=label),
-            models.Module.objects.filter(project_version__version_number=label),
-            models.Klass.objects.filter(module__project_version__version_number=label),
+            models.Module.objects.filter(
+                project_version__version_number=label
+            ).order_by("name"),
+            models.Klass.objects.filter(
+                module__project_version__version_number=label
+            ).order_by("module__name", "name"),
             models.KlassAttribute.objects.filter(
                 klass__module__project_version__version_number=label
-            ),
+            ).order_by("klass__module__name", "klass__name", "name"),
             models.Method.objects.filter(
                 klass__module__project_version__version_number=label
-            ),
+            ).order_by("klass__module__name", "klass__name", "name"),
             models.Inheritance.objects.filter(
                 parent__module__project_version__version_number=label
-            ),
+            ).order_by("child__module__name", "child__name", "order"),
         )
         objects = list(chain.from_iterable(querysets))
         for obj in objects:
