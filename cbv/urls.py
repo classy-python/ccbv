d275386ae1028bc591961efc090a5f32458f3461
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-08-21 01:42:48 +0200

    black reformatting

--- cbv/urls.py
+++ cbv/urls.py
@@ -21,54 +21,77 @@ from cbv import views
 
 urlpatterns = [
     path("", RedirectView.as_view(url=reverse_lazy("home"))),
-    path("<slug:package>/", include([
-        path(
-            "",
-            views.RedirectToLatestVersionView.as_view(),
-            {"url_name": "version-detail"},
+    path(
+        "<slug:package>/",
+        include(
+            [
+                path(
+                    "",
+                    views.RedirectToLatestVersionView.as_view(),
+                    {"url_name": "version-detail"},
+                ),
+                path(
+                    "latest/",
+                    include(
+                        [
+                            path(
+                                "",
+                                views.RedirectToLatestVersionView.as_view(),
+                                {"url_name": "version-detail"},
+                                name="latest-version-detail",
+                            ),
+                            path(
+                                "<str:module>/",
+                                include(
+                                    [
+                                        path(
+                                            "",
+                                            views.RedirectToLatestVersionView.as_view(),
+                                            {"url_name": "module-detail"},
+                                            name="latest-module-detail",
+                                        ),
+                                        path(
+                                            "<str:klass>/",
+                                            views.RedirectToLatestVersionView.as_view(),
+                                            {"url_name": "klass-detail"},
+                                            name="latest-klass-detail",
+                                        ),
+                                    ]
+                                ),
+                            ),
+                        ]
+                    ),
+                ),
+                path(
+                    "<str:version>/",
+                    include(
+                        [
+                            path(
+                                "",
+                                views.VersionDetailView.as_view(),
+                                name="version-detail",
+                            ),
+                            path(
+                                "<str:module>/",
+                                include(
+                                    [
+                                        path(
+                                            "",
+                                            views.ModuleDetailView.as_view(),
+                                            name="module-detail",
+                                        ),
+                                        path(
+                                            "<str:klass>/",
+                                            views.KlassDetailView.as_view(),
+                                            name="klass-detail",
+                                        ),
+                                    ]
+                                ),
+                            ),
+                        ]
+                    ),
+                ),
+            ]
         ),
-        path(
-            "latest/", include([
-            path(
-                "",
-                views.RedirectToLatestVersionView.as_view(),
-                {"url_name": "version-detail"},
-                name="latest-version-detail",
-            ),
-            path("<str:module>/", include([
-                path(
-                    "",
-                    views.RedirectToLatestVersionView.as_view(),
-                    {"url_name": "module-detail"},
-                    name="latest-module-detail"
-                ),
-                path(
-                    "<str:klass>/",
-                    views.RedirectToLatestVersionView.as_view(),
-                    {"url_name": "klass-detail"},
-                    name="latest-klass-detail",
-                )
-            ])),
-        ])),
-        path(
-            "<str:version>/", include([
-            path(
-                "",
-                views.VersionDetailView.as_view(),
-                name="version-detail",
-            ),
-            path("<str:module>/", include([
-                path(
-                    "",
-                    views.ModuleDetailView.as_view(),
-                    name="module-detail"
-                ),
-                path(
-                    "<str:klass>/",
-                    views.KlassDetailView.as_view(),
-                    name="klass-detail",
-                )
-            ])),
-        ]))
-    ]))
+    ),
 ]
