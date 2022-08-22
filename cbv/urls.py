9fb398fdbc4024e6d19505e7053ae34fedb1322a
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-08-23 00:36:32 +0200

    towards flat path(..)s

--- cbv/urls.py
+++ cbv/urls.py
@@ -12,7 +12,7 @@ django/1.41a/core
 django/1.41a/core/DjangoRuntimeWarning
 """
 
-from django.urls import include, path, reverse_lazy
+from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
 
 from cbv import views
@@ -22,75 +22,40 @@ urlpatterns = [
     path("", RedirectView.as_view(url=reverse_lazy("home"))),
     path(
         "<slug:package>/",
-        include(
-            [
-                path(
-                    "",
-                    views.RedirectToLatestVersionView.as_view(),
-                    {"url_name": "version-detail"},
-                ),
-                path(
-                    "latest/",
-                    include(
-                        [
-                            path(
-                                "",
-                                views.RedirectToLatestVersionView.as_view(),
-                                {"url_name": "version-detail"},
-                                name="latest-version-detail",
-                            ),
-                            path(
-                                "<str:module>/",
-                                include(
-                                    [
-                                        path(
-                                            "",
-                                            views.RedirectToLatestVersionView.as_view(),
-                                            {"url_name": "module-detail"},
-                                            name="latest-module-detail",
-                                        ),
-                                        path(
-                                            "<str:klass>/",
-                                            views.RedirectToLatestVersionView.as_view(),
-                                            {"url_name": "klass-detail"},
-                                            name="latest-klass-detail",
-                                        ),
-                                    ]
-                                ),
-                            ),
-                        ]
-                    ),
-                ),
-                path(
-                    "<str:version>/",
-                    include(
-                        [
-                            path(
-                                "",
-                                views.VersionDetailView.as_view(),
-                                name="version-detail",
-                            ),
-                            path(
-                                "<str:module>/",
-                                include(
-                                    [
-                                        path(
-                                            "",
-                                            views.ModuleDetailView.as_view(),
-                                            name="module-detail",
-                                        ),
-                                        path(
-                                            "<str:klass>/",
-                                            views.KlassDetailView.as_view(),
-                                            name="klass-detail",
-                                        ),
-                                    ]
-                                ),
-                            ),
-                        ]
-                    ),
-                ),
-            ]
-        ),
+        views.RedirectToLatestVersionView.as_view(),
+        {"url_name": "version-detail"},
+    ),
+    path(
+        "<slug:package>/latest/",
+        views.RedirectToLatestVersionView.as_view(),
+        {"url_name": "version-detail"},
+        name="latest-version-detail",
+    ),
+    path(
+        "<slug:package>/<str:version>/",
+        views.VersionDetailView.as_view(),
+        name="version-detail",
+    ),
+    path(
+        "<slug:package>/latest/<str:module>/",
+        views.RedirectToLatestVersionView.as_view(),
+        {"url_name": "module-detail"},
+        name="latest-module-detail",
+    ),
+    path(
+        "<slug:package>/<str:version>/<str:module>/",
+        views.ModuleDetailView.as_view(),
+        name="module-detail",
+    ),
+    path(
+        "<slug:package>/latest/<str:module>/<str:klass>/",
+        views.RedirectToLatestVersionView.as_view(),
+        {"url_name": "klass-detail"},
+        name="latest-klass-detail",
+    ),
+    path(
+        "<slug:package>/<str:version>/<str:module>/<str:klass>/",
+        views.KlassDetailView.as_view(),
+        name="klass-detail",
     ),
 ]
