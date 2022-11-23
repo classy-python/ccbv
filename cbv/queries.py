dc9b5a9f9e9fab1d74b84874f219a06421d619bc
Charlie Denton <charlie@meshy.co.uk>
2022-11-23 00:35:16 +0000

    Extract query to named variable

--- cbv/queries.py
+++ cbv/queries.py
@@ -85,11 +85,12 @@ class NavBuilder:
                 for other_version in other_versions
             ]
 
+        module_set = projectversion.module_set.prefetch_related("klass_set").order_by(
+            "name"
+        )
         modules = [
             self._to_module_data(module=m, active_module=module, active_klass=klass)
-            for m in projectversion.module_set.prefetch_related("klass_set").order_by(
-                "name"
-            )
+            for m in module_set
         ]
 
         nav_data = NavData(
